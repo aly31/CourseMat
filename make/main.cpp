b29db783c9f6f44451ad65caf35a441567bf3dc1
Jaber hasbestan <hasbesta@br018.pvt.bridges.psc.edu>
2018-10-16 13:33:04 -0400

    oct_16

---
+++ make/main.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include "chunkedArray.h"
+
+
+int main( int argcs, char *pArgs[] )
+{
+
+ChunkedArray A(10,10,10);
+
+
+printf("Hi \n");
+//printf("Good Job! \n");
+
+}
+
+

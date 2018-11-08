f65f8426b534472f89e293de2c1929559dac33ac
Jaber hasbestan <hasbesta@br005.pvt.bridges.psc.edu>
2018-11-07 20:46:49 -0500

    minor err

--- devicePop.cu
+++ devicePop.cu
@@ -20,10 +20,10 @@ for(int i=0;i<count;i++)
 cudaGetDeviceProperties(&prop,i);
 
 printf("=======================================");
-printf("name= %d\n",prop.name);
+printf("name= %s\n",prop.name);
 printf(" compute cap= %d\n",prop.major);
 printf(" GlobalMem = %d\n",prop.totalGlobalMem);
-printf(" multi peocess count = %d\n",prop.multiProcessorCount);
+printf(" multi peocess count = %ld\n",prop.multiProcessorCount);
 printf(" max thread/block = %d\n",prop.maxThreadsPerBlock);
 
 
